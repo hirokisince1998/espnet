f647803ecf077970421638144dd44556177c7389
hirokisince1998 <hiroki@speech-lab.org>
2024-01-26 18:52:44 +0900

    model for inference (train.total_count.ave.pth)

--- egs2/uudb/tts1/run.sh
+++ egs2/uudb/tts1/run.sh
@@ -1,3 +1,2 @@
-./tts.sh --fs 22050 --cleaner none --g2p none --tts_task gan_tts --use_sid true --train_set train --valid_set test --test_sets test --srctexts data/train/text --train_config conf/train.yaml --use_emodims true "$@"
-
+./tts.sh --fs 22050 --cleaner none --g2p none --tts_task gan_tts --use_sid true --train_set train --valid_set test --test_sets test --srctexts data/train/text --train_config conf/train.yaml --use_emodims true  --inference_model train.total_count.ave.pth "$@"
 #python3 -m espnet2.bin.tts_inference --ngpu 0 --data_path_and_name_and_type dump/raw/test/text,text,text --data_path_and_name_and_type dump/raw/test/utt2emodim,emodims,csv_float --model_file $modeldir/latest.pth --train_config $modeldir/config.yaml --output_dir /home/drobo/hiroki/vits-pic --config conf/decode.yaml --data_path_and_name_and_type test/utt2sid,sids,text_int
